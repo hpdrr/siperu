2055b6a4f894df93725803a825a00bee87b451d4
Hapidr <hapidr24@gmail.com>
2023-12-02 19:10:16 +0700

    [Login Page]: Add purple login page with image on the left

---
+++ resources/views/login.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.home')
+
+@section('title', 'Login')
+@section('container')
+  <!-- Container Start -->
+  <div class="container" id="screen">
+    <div class="row">
+      <!--row Start-->
+      <div class="col-lg-6 login-image d-none d-lg-block my-auto">
+        <img src="{{ asset('assets/img/login-image.png') }}" class="w-100" alt="Labor Software Engineering" />
+      </div>
+      <div class="col-lg-6 my-auto">
+        <!--col-lg-6 Start-->
+        <div id="login">
+          <form action="#">
+            <!-- Form Start -->
+            <h4>Selamat datang di Siperu</h4>
+            <p class="text-muted">Harap masuk untuk menggunakan Siperu</p>
+            <div class="form-floating mb-3">
+              <input type="email" class="form-control" id="floatingInput" placeholder="Enter Email" required />
+              <label for="floatingInput" class="text-muted">Nama Pengguna</label>
+            </div>
+            <div class="form-floating mb-2">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Enter Password" required />
+              <label for="floatingPassword" class="text-muted">Kata Sandi</label>
+            </div>
+            <button type="submit" class="btn btn-login">Masuk</button>
+          </form>
+          <!-- Form End -->
+        </div>
+      </div>
+      <!--col-lg-6 End-->
+    </div>
+    <!--row End-->
+  </div>
+  <!-- Container End-->
+@endsection
